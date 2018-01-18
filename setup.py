87cd5640544d56e3fb06b81738469556a4ebcd9e
Danila Kulakov <kdanilke@gmail.com>
2018-01-18 10:13:26 +0100

    fix python versions conflict #2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_counter_field_py3',
-    version='0.0.1',
+    version='0.0.1.2',
     packages=find_packages(exclude=['tests']),
     license='MIT License',
     description='python3 fork of django-counter-field',
