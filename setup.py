faaa099f1a78b9ff1669b9f130000348f6f9f0da
yongwookim <yongwookim@users.noreply.github.com>
2019-04-09 11:01:27 +0900

    updating for pip 10 changes

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
